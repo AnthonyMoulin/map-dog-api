b1d97e29df17e055d799f2eccc783fc0fe0d6626
AnthonyMoulin <antmoulin1@gmail.com>
2017-08-08 15:32:39 +0200

    Sync project

--- src/map-dog.sql
+++ src/map-dog.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `map-dog`
+-- Base de données :  "map-dog"
 --
 
 -- --------------------------------------------------------
@@ -26,39 +26,39 @@ SET time_zone = "+00:00";
 -- Structure de la table `dog`
 --
 
-CREATE TABLE `dog` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` char(32) NOT NULL,
-  `avatar` char(32) NOT NULL
+CREATE TABLE "dog" (
+  "id" int(10) UNSIGNED NOT NULL,
+  "name" char(32) NOT NULL,
+  "avatar" char(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `dog`
 --
 
-INSERT INTO `dog` (`id`, `name`, `avatar`) VALUES
-(146, 'foo', 'sparky'),
-(147, 'bar', 'sparky'),
-(148, 'baz', 'sparky');
+INSERT INTO "dog" ("id", "name", "avatar") VALUES
+(146, "foo", "sparky"),
+(147, "bar", "sparky"),
+(148, "baz", "sparky");
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `map`
+-- Structure de la table "map"
 --
 
-CREATE TABLE `map` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `lat` float NOT NULL DEFAULT '0',
-  `lng` float NOT NULL DEFAULT '0',
-  `time` int(11) NOT NULL
+CREATE TABLE "map" (
+  "id" int(10) UNSIGNED NOT NULL,
+  "lat" float NOT NULL DEFAULT '0',
+  "lng"  float NOT NULL DEFAULT '0',
+  "time" int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `map`
+-- Contenu de la table "map"
 --
 
-INSERT INTO `map` (`id`, `lat`, `lng`, `time`) VALUES
+INSERT INTO "map" ("id", "lat", "lng", "time") VALUES
 (146, 2, 2, 0),
 (147, 0, 0, 0),
 (148, 0, 0, 0);
@@ -68,36 +68,36 @@ INSERT INTO `map` (`id`, `lat`, `lng`, `time`) VALUES
 --
 
 --
--- Index pour la table `dog`
+-- Index pour la table "dog"
 --
-ALTER TABLE `dog`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
+ALTER TABLE "dog"
+  ADD PRIMARY KEY ("id"),
+  ADD UNIQUE KEY "name" ("name");
 
 --
--- Index pour la table `map`
+-- Index pour la table "map"
 --
-ALTER TABLE `map`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE "map"
+  ADD PRIMARY KEY ("id");
 
 --
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT pour la table `dog`
+-- AUTO_INCREMENT pour la table "dog"
 --
-ALTER TABLE `dog`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
+ALTER TABLE "dog"
+  MODIFY "id" int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
 --
 -- Contraintes pour les tables exportées
 --
 
 --
--- Contraintes pour la table `map`
+-- Contraintes pour la table "map"
 --
-ALTER TABLE `map`
-  ADD CONSTRAINT `map_ibfk_1` FOREIGN KEY (`id`) REFERENCES `dog` (`id`);
+ALTER TABLE "map"
+  ADD CONSTRAINT "map_ibfk_1" FOREIGN KEY ("id") REFERENCES "dog" ("id");
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
